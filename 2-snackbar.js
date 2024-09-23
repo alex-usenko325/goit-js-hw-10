fd31f7978b73d1b6129bf240912858983772422e
alex-usenko325 <alex.usenko.97@gmail.com>
2024-09-23 11:34:42 +0000

    Deploying to gh-pages from @ alex-usenko325/goit-js-hw-10@24a55e88ddc92915a6506266bf1e3290972782c9 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{i}from"./assets/vendor-BbbuE1sJ.js";document.querySelector(".form").addEventListener("submit",function(t){t.preventDefault();const s=Number(t.target.elements.delay.value),o=t.target.elements.state.value;new Promise((e,m)=>{setTimeout(()=>{o==="fulfilled"?e(s):m(s)},s)}).then(e=>{i.success({title:"",message:`Fulfilled promise in ${e}ms`,position:"topRight"})}).catch(e=>{i.error({title:"",message:`Rejected promise in ${e}ms`,position:"topRight"})})});
+//# sourceMappingURL=2-snackbar.js.map
